be56c6179cef54863bee1493927f0c4cef022219
Ozi-Technology <tech@ozi.in>
2026-07-30 00:07:19 +0530

    newTest

--- src/utils/fileStore.js
+++ src/utils/fileStore.js
@@ -16,5 +16,5 @@ const writeData = (filename, data) => {
   const filePath = path.join(__dirname, '../../data/', filename);
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
 };
-
+//some changes are made here
 module.exports = { readData, writeData };
